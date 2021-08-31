19fb67431f47f4174ee3d5f5fb9be5d0143cc185
fengzhenbing <fengzhenbing98@126.com>
2021-09-01 00:15:32 +0800

    Add the list of contributors automatically. (#223)

---
+++ src/components/Contributors.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+
+export default () => {
+  const [contributers, setContributers] = useState([]);
+  useEffect(() => {
+    fetch("https://api.github.com/repos/apache/incubator-shenyu/contributors?page=1&per_page=10000").then(function (response) {
+        return response.json();
+    }).then((res)=>{
+      setContributers(res);
+    });
+  });
+  let html = "<table>";
+  if (contributers && Array.isArray(contributers)) {
+    contributers.forEach((c, i) => {
+        if (i % 5 === 0) {
+            if (i > 0) {
+                html += "</tr>"
+            }
+            html += "<tr>"
+        }
+        html += `<td><a href="${c.html_url}" target="_blank"><img src="${c.avatar_url}" height="20" /> @${c.login}</a></td>`;
+        if (i === contributers.length - 1) {
+            html += "</tr>"
+        }
+    })
+  }
+  html+="</table>"
+  return <div dangerouslySetInnerHTML={{__html: html}} />;
+}

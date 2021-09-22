c1e23b4153b80a2a0c8d14caec17ace924e334da
YuI <13451528+HessTina-YuI@users.noreply.github.com>
2021-09-23 01:16:13 +0800

    add new download page, modify contributors for adding dashboard and website's developer. (#289)
    * add: wechat and qq's QR Code
    
    * fix: the markdownline error
    
    * fix: the markdownline error
    
    * modift: Download button change into Github
    
    * modift: Download markdown file name.
    
    * add: new download page.
    
    * modify: contributors add dashboard and website's developer.
    
    Co-authored-by: 艺铭 <yi.liu@perfma.com>

--- src/components/Contributors.tsx
+++ src/components/Contributors.tsx
@@ -1,31 +1,38 @@
 import React, { useState, useEffect } from 'react';
 
-export default () => {
-  const [contributers, setContributers] = useState([]);
-  useEffect(() => {
-    if(!contributers || contributers.length === 0) {
-      fetch("https://api.github.com/repos/apache/incubator-shenyu/contributors?page=1&per_page=10000").then(function (response) {
-        return response.json();
-      }).then((res)=>{
-        setContributers(res);
-      });
-    }
-  });
-  let html = "<table>";
-  if (contributers && Array.isArray(contributers)) {
-    contributers.forEach((c, i) => {
-        if (i % 5 === 0) {
-            if (i > 0) {
-                html += "</tr>"
+export default (props) => {
+    const [contributers, setContributers] = useState([]);
+    useEffect(() => {
+        const repo = props.repo ?? 'apache/incubator-shenyu';
+
+        if (!contributers || contributers.length === 0) {
+            fetch(`https://api.github.com/repos/${repo}/contributors?page=1&per_page=10000`).then(function (response) {
+                return response.json();
+            }).then((res) => {
+                setContributers(res);
+            });
+        }
+    });
+    let html = '<table>';
+    if (contributers && Array.isArray(contributers)) {
+        contributers.forEach((c, i) => {
+            if (i % 5 === 0) {
+                if (i > 0) {
+                    html += '</tr>';
+                }
+                html += '<tr>';
             }
-            html += "<tr>"
-        }
-        html += `<td><a href="${c.html_url}" target="_blank"><img src="${c.avatar_url}" height="20" /> @${c.login}</a></td>`;
-        if (i === contributers.length - 1) {
-            html += "</tr>"
-        }
-    })
-  }
-  html+="</table>"
-  return <div dangerouslySetInnerHTML={{__html: html}} />;
+            html += `<td>
+                        <a href="${c.html_url}" target="_blank">
+                            <img src="${c.avatar_url}" height="20" /> 
+                            <span style={{ whiteSpace: 'nowrap' }}>@${c.login}</span>
+                        </a>
+                    </td>`;
+            if (i === contributers.length - 1) {
+                html += '</tr>';
+            }
+        });
+    }
+    html += '</table>';
+    return <div dangerouslySetInnerHTML={{ __html: html }}/>;
 }

fbfdcf9ff1cc168a54d00bac3bc2d87dffebe7e2
gzx_huaweiPc <gouzixing@qq.com>
2022-07-04 01:59:39 +0800

    blog page refractor

---
+++ src/data/blogInfo.js
@@ -0,0 +1,144 @@
+export default [
+    {
+        categray: 'DataSync',
+        posts: [
+            {
+                title: "Etcd Data Synchronization Source Code Analysis",
+                author: "4zd",
+                src: "DataSync-SourceCode-Analysis-Etcd-Data-Sync",
+                cover: "/img/blog/1-1.png",
+                date: '2021-07-2',
+            },
+            {
+                title: "Http Long Polling Data Synchronization Source Code Analysis",
+                author: "midnight2104",
+                src: "DataSync-SourceCode-Analysis-Http-Data-Sync",
+                cover: "/img/blog/1-2.png",
+                date: '2022-07-2',
+            },
+            {
+                title: "Nacos Data Synchronization Source Code Analysis",
+                author: "4zd",
+                src: "DataSync-SourceCode-Analysis-Nacos-Data-Sync",
+                cover: "/img/blog/1-3.png",
+                date: '2022-07-2',
+            },
+            {
+                title: "WebSocket Data Synchronization Source Code Analysis",
+                author: "midnight2104",
+                src: "DataSync-SourceCode-Analysis-WebSocket-Data-Sync",
+                cover: "/img/blog/1-4.png",
+                date: '2022-07-2',
+            },
+            {
+                title: "ZooKeeper Data Synchronization Source Code Analysis",
+                author: "midnight2104",
+                src: "DataSync-SourceCode-Analysis-ZooKeeper-Data-Sync",
+                cover: "/img/blog/1-5.png",
+                date: '2022-07-2',
+            },
+        ]
+    },
+    {
+        categray: 'Integration Test',
+        posts: [
+            {
+                title: "Integration Test Analysis",
+                author: "Kunshuai Zhu",
+                src: "IntegrationTest-Analysis",
+                cover: "/img/logo.svg",
+                date: '2022-07-2',
+            },
+        ]
+    },
+    {
+        categray: 'Plugin',
+        posts: [
+            {
+                title: "Code Analysis For Context-Path Plugin",
+                author: "Kunshuai Zhu",
+                src: "Plugin-SourceCode-Analysis-Context-Path-Plugin",
+                cover: "/img/logo.svg",
+                date: '2021-07-2',
+            },
+            {
+                title: "Code Analysis For Dubbo Plugin",
+                author: "midnight2104",
+                src: "Plugin-SourceCode-Analysis-Dubbo-Plugin",
+                cover: "/img/blog/3-2.png",
+                date: '2021-07-2',
+            },
+            {
+                title: "Code Analysis For Param-Mapping Plugin",
+                author: "Kunshuai Zhu",
+                src: "Plugin-SourceCode-Analysis-Param-Mapping-Plugin",
+                cover: "/img/blog/3-3.jpg",
+                date: '2021-07-2',
+            },
+            {
+                title: "Code Analysis For Divide Plugin",
+                author: "midnight2104",
+                src: "Plugin-SourceCode-Analysis-Divide-Plugin",
+                cover: "/img/blog/3-4.png",
+                date: '2021-07-2',
+            },
+        ]
+    },
+    {
+        categray: 'SPI',
+        posts: [
+            {
+                title: "MatchStrategy -- analyze the design based on SPI",
+                author: "Huihui Yin",
+                src: "SPI-SourceCode-Analysis-MatchStrategy-SPI",
+                cover: "/img/blog/4-1.png",
+                date: '2021-07-2',
+            },
+            {
+                title: "PredicateJudge -- analyze the design based on SPI",
+                author: "Huihui Yin",
+                src: "SPI-SourceCode-Analysis-PredicateJudge-SPI",
+                cover: "/img/blog/4-2.png",
+                date: '2021-07-2',
+            },
+            {
+                title: "LoadBalance SPI Source Code Analysis",
+                author: "Huihui Yin",
+                src: "SPI-SourceCode-Analysis-LoadBalance-SPI",
+                cover: "/img/blog/4-3.png",
+                date: '2021-07-2',
+            },
+            {
+                title: "RateLimiter SPI code analysis",
+                author: "Huihui Yin",
+                src: "SPI-SourceCode-Analysis-RateLimiter-SPI",
+                cover: "/img/blog/4-4.png",
+                date: '2021-07-2',
+            },
+        ]
+    },
+    {
+        categray: 'RegisterCenter',
+        posts: [
+            {
+                title: "Register Center Source Code Analysis of Http Register",
+                author: "midnight2104",
+                src: "RegisterCenter-SourceCode-Analysis-Http-Register",
+                cover: "/img/blog/5-1.png",
+                date: '2021-07-2',
+            },
+        ]
+    },
+    {
+        categray: 'Start',
+        posts: [
+            {
+                title: "Apache ShenYu Start Demo",
+                author: "Kunshuai Zhu",
+                src: "Start-SourceCode-Analysis-Start-Demo",
+                cover: "/img/blog/6-1.png",
+                date: '2021-07-2',
+            },
+        ]
+    }
+]
---
+++ src/pages/blog.tsx
@@ -0,0 +1,83 @@
+
+import React, { useEffect, useState } from 'react';
+import Layout from '@theme/Layout';
+import styles from './blog.module.css';
+import blogInfo from '../data/blogInfo';
+
+function Blog() {
+    const [activeAnchor, setActiveAnchor] = useState(0);
+    let list = document.getElementsByClassName('catalogueBoxA_src-pages-blog-module');
+    let contentList = document.getElementsByClassName('title_src-pages-blog-module')
+
+    useEffect(()=>{
+        for(let i=0; i<list.length; i++){
+            if(i==activeAnchor){
+                list[i].setAttribute('style','color: var(--ifm-color-primary);')
+            }
+            else{
+                list[i].setAttribute('style','color: var(--ifm-toc-link-color);')
+            }
+        }
+    },[activeAnchor])
+
+    useEffect(() => {
+        window.addEventListener('scroll', ()=>{
+            // let scrollDistance = parseInt(document.documentElement.scrollTop || document.body.scrollTop || 0, 10);
+            // console.log(scrollDistance)
+            for(let i=0; i<contentList.length; i++){
+                let dist = contentList[i].getBoundingClientRect().y
+                // console.log('di',dist)
+                if(  dist >= 90 && dist <=300){
+                    setActiveAnchor(i)
+                }
+            }
+        });
+        return () => window.removeEventListener('scroll',()=>{});
+      }, []);
+
+    return (
+        <Layout title="Blog">
+            <div className={styles.container}>
+                <div className={styles.content}>
+                    {
+                        blogInfo.map((item, key) => {
+                            return (
+                                <div className={styles.categray} key={key}>
+                                    <div className={styles.darkAnchor} id={item.categray}></div>
+                                    <div className={styles.title}>{item.categray} ————</div>
+                                    {item.posts.map((post, key) => {
+                                        return (
+                                            <div className={styles.card} key={key}>
+                                                <img className={styles.cardImage} src={post.cover}  width='100%' height={140}></img>
+                                                <div className={styles.postTitle}>{post.title}</div>
+                                                <div className={styles.author}> {post.author}   &ensp; &ensp;     {post.date} </div>
+                                                <button className={styles.read} onClick={() => window.location.href = "http://localhost:3000/blog/" + post.src}> >>Read More </button>
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <div className={styles.catalogue}>
+                    <div className={styles.catalogueBox}>                 
+                    <ul className={styles.catalogueBoxUl}>
+                    {
+                        blogInfo.map((item, index) => {
+                            const route = '#' + item.categray
+                            const id = 'Cata' + item.categray
+                            return (
+                                <li key={index} className={styles.catalogueBoxLi} id={id} ><a href={route} id='catalogueBoxA' className={styles.catalogueBoxA}  onClick={()=>setActiveAnchor(index)}>{item.categray}</a></li>
+                            )
+                        })
+                    }
+                    </ul>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
+}
+
+export default Blog;

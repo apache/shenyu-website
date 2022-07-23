795467417241cdb9fb529c48b310aa08b1d17533
dengliming <liming.d.pro@gmail.com>
2022-07-23 22:23:43 +0800

    test

--- .github/scripts/conf.py
+++ .github/scripts/conf.py
@@ -16,7 +16,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Apache ShenYu document'
-copyright = '2021, Apache ShenYu'
+copyright = '2022, Apache ShenYu'
 author = 'Apache ShenYu'
 
 
@@ -90,7 +90,7 @@ latex_elements = {
 \usepackage[draft]{minted}
 \setcounter{tocdepth}{3}
 \usepackage{xurl}
-\usepackage{bmpsize}
+\usepackage{graphicx}
     ''',
     'fvset': r'''
 \fvset{fontsize=\small}

cf8f13fa68fc5559e46b32549d006cdc2a33179b
lishuo <lishuo@apache.org>
2022-10-18 16:14:56 +0800

    [type: feature] add shenyu client golang release event (#804)

--- src/data/event.js
+++ src/data/event.js
@@ -3,6 +3,11 @@ import Translate from "@docusaurus/Translate";
 
 export default [
     {
+        title: <Translate>Release Apache Shenyu client golang 1.0.0</Translate>,
+        description: "New Features  \n  1. Support ShenYu Admin with http type Register to ShenYu. \n  2. Support use Nacos type Register to ShenYu. \n  3. Support use Consul type Register to ShenYu. \n  ......",
+        src:'client-golang-1.0.0-release',
+    },
+    {
         title: <Translate>Release Apache Shenyu 2.5.0</Translate>,
         description: "New Features  \n  1. Add logging-aliyun-sls plugin. \n  2. Add mock plugin. \n  3. Add logging-es plugin. \n  ......",
         src:'2.5.0-release',
@@ -13,27 +18,27 @@ export default [
         src:'2.4.3-release',
     },
     {
-        title: <Translate>Release Apache Shenyu 2.4.2</Translate>, 
+        title: <Translate>Release Apache Shenyu 2.4.2</Translate>,
         description: "New Features  \n  1. Add Mqtt plugin.  \n  2. Add Shenyu-Agent module support observability.  \n  3. Add opentelemetry plugin on Shenyu-Agent module.  \n  ......",
         src:'2.4.2-release',
     },
-    {        
-        title: <Translate>Release Apache Shenyu 2.4.1</Translate>, 
+    {
+        title: <Translate>Release Apache Shenyu 2.4.1</Translate>,
         description: "New Features  \n  1. Support PostgreSQL for admin.  \n  2. Support dynamic loading plugin.  \n  3. Support local modification data mode.  \n  ......",
         src:'2.4.1-release',
     },
-    {        
-        title: <Translate>Release Apache Shenyu 2.4.0</Translate>, 
+    {
+        title: <Translate>Release Apache Shenyu 2.4.0</Translate>,
         description: "New Features  \n  1. Support reading init_script file which is not under resource/directory.  \n  2. Display the plugin menus in categories.  \n  3. Admin add execute Multi-path sql script.  \n  ......",
         src:'2.4.0-release',
     },
-    {        
-        title: <Translate>Release Apache Shenyu 2.3.0</Translate>, 
+    {
+        title: <Translate>Release Apache Shenyu 2.3.0</Translate>,
         description: "soul-admin  \n  1. Add open field to allow app path authentication or not in sign plugin.  \n  2. Optimize divide plugin to use common plugin template in soul-dashboard.  \n  3. Add default values and rule checks in plugin handler.  \n  ......",
         src:'2.3.0-release',
     },
-    {        
-        title: <Translate>Release Apache Shenyu 2.2.0</Translate>, 
+    {
+        title: <Translate>Release Apache Shenyu 2.2.0</Translate>,
         description: "New Features  \n  1. Complete plug-in architecture design, plug-in hot-swappable.  \n  2. Fully supports all versions of dubbo, alibaba-dubbo, apache-dubbo.  \n  3. Support dubbo generalization call, multi-parameter, complex parameter interface.  \n  ......",
         src:'2.2.0-release',
     }

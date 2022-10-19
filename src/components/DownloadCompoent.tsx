764f90e34b74e49b677b622d48a0fc808041405f
hgaol <dhangao@hotmail.com>
2022-10-19 22:36:28 +0800

    Add docs about download for dotnet and golang client

--- src/components/DownloadCompoent.tsx
+++ src/components/DownloadCompoent.tsx
@@ -4,8 +4,9 @@ import Translate, { translate } from '@docusaurus/Translate';
 import styles from './DownloadCompoent.module.css';
 
 const data = [
-    {
-        title: translate({ message: 'Source Codes' }),
+    [{
+        title: translate({ message: 'ShenYu Source Codes' }),
+        index: 0,
         versions: [
             {
                 versionTitle: '2.5.0',
@@ -34,7 +35,8 @@ const data = [
         ],
     },
     {
-        title: translate({ message: 'ShenYu-Admin Binary Distribution' }), 
+        title: translate({ message: 'ShenYu-Admin Binary Distribution' }),
+        index: 1,
         versions: [
             {
                 versionTitle: '2.5.0',
@@ -63,7 +65,8 @@ const data = [
         ],
     },
     {
-        title: translate({ message: 'ShenYu-Bootstrap Binary Distribution' }), 
+        title: translate({ message: 'ShenYu-Bootstrap Binary Distribution' }),
+        index: 2,
         versions: [
             {
                 versionTitle: '2.5.0',
@@ -90,30 +93,58 @@ const data = [
                 }
             }
         ],
-    }
+    }],
+    [{
+        title: translate({ message: 'ShenYu client golang Source Codes' }),
+        index: 3,
+        versions: [
+            {
+                versionTitle: '1.0.0',
+                targets: {
+                    'tar': 'https://www.apache.org/dyn/closer.lua/shenyu/shenyu-client-golang/v1.0.0/shenyu-client-golang-v1.0.0-src.tar.gz',
+                    'asc': 'https://downloads.apache.org/shenyu/shenyu-client-golang/v1.0.0/shenyu-client-golang-v1.0.0-src.tar.gz.asc',
+                    'sha512': 'https://downloads.apache.org/shenyu/shenyu-client-golang/v1.0.0/shenyu-client-golang-v1.0.0-src.tar.gz.sha512',
+                }
+            }
+        ],
+    }],
+    [{
+        title: translate({ message: 'ShenYu client .NET Source Codes' }),
+        index: 4,
+        versions: [
+            {
+                versionTitle: '1.0.0',
+                targets: {
+                    'tar': 'https://www.apache.org/dyn/closer.lua/shenyu/shenyu-client-dotnet/v1.0.0/shenyu-client-dotnet-v1.0.0-src.tar.gz',
+                    'asc': 'https://downloads.apache.org/shenyu/shenyu-client-dotnet/v1.0.0/shenyu-client-dotnet-v1.0.0-src.tar.gz.asc',
+                    'sha512': 'https://downloads.apache.org/shenyu/shenyu-client-dotnet/v1.0.0/shenyu-client-dotnet-v1.0.0-src.tar.gz.sha512',
+                }
+            }
+        ],
+    }]
 ]
 
 function DownloadCompoent() {
-    const [showItem, setShowItem] = useState([false, false, false]);
+    const [showItem, setShowItem] = useState([false, false, false, false, false]);
 
-    const closeList = useCallback((e)=>{
-        const {target} = e;
+    const closeList = useCallback((e) => {
+        const { target } = e;
         const value = target.getAttributeNode('id')?.value;
         if (value && value === 'dropDownButton') {
             return
         } else {
-            setShowItem([false,false,false]);
+            setShowItem([false, false, false, false, false]);
         }
     }, [])
 
-    useEffect(()=>{
-        document.addEventListener('click', (e)=>{closeList(e)});
-        return document.removeEventListener('click',(e)=>{closeList(e)});
+    useEffect(() => {
+        document.addEventListener('click', (e) => { closeList(e) });
+        return document.removeEventListener('click', (e) => { closeList(e) });
     }, []);
 
     const showList = useCallback(index => {
         const newShowItem = showItem.map((item, key) => {
-            if (key === index){
+            if (key === index) {
                 return !item;
             } else {
                 return false;
@@ -121,48 +152,53 @@ function DownloadCompoent() {
         })
         setShowItem([...newShowItem]);
     }, [showItem]);
-    
+
     return (
         <div className={styles.downloadContainer}>
-            {data.map((item, index) => {
-                const { versions, title } = item;
+            {data.map((project, index) => {
                 return (
-                    <div className={styles.downloadCard}>
-                        <div className={styles.downloadCardTitle}>{title}</div>
-                        <button id= 'dropDownButton' className={styles.downloadCardButton} onClick={() => { showList(index) }}><Translate>Source</Translate></button>
-                        {
-                            showItem[index] &&
-                            (
-                                <div className={styles.dropDownContainer}>
+                    <div key={index} className={styles.downloadRowContainer}>
+                        {project.map((item, index) => {
+                            const { versions, title } = item;
+                            return (
+                                <div key={item.index} className={styles.downloadCard}>
+                                    <div className={styles.downloadCardTitle}>{title}</div>
+                                    <button id='dropDownButton' className={styles.downloadCardButton} onClick={() => { showList(item.index) }}><Translate>Source</Translate></button>
                                     {
-                                        versions.map(item2 => {
-                                            const { versionTitle, targets } = item2;
-                                            const listArr = Object.keys(targets).map(list => {
-                                                return {
-                                                    title: list,
-                                                    link: targets[list],
+                                        showItem[item.index] &&
+                                        (
+                                            <div className={styles.dropDownContainer}>
+                                                {
+                                                    versions.map((item2, index2) => {
+                                                        const { versionTitle, targets } = item2;
+                                                        const listArr = Object.keys(targets).map(list => {
+                                                            return {
+                                                                title: list,
+                                                                link: targets[list],
+                                                            }
+                                                        })
+                                                        return (
+                                                            <div key={index2}>
+                                                                <div className={styles.versionTitle}>{versionTitle}</div>
+                                                                <div className={styles.downloadLinks}>
+                                                                    {listArr.map((url, ind3) => {
+                                                                        const title = '[' + url.title + ']'
+                                                                        return (
+                                                                            <a key={ind3} href={url.link}>{title}</a>
+                                                                        )
+                                                                    })}
+                                                                </div>
+                                                            </div>
+                                                        )
+                                                    })
                                                 }
-                                            })
-                                            return (
-                                                <div>
-                                                    <div className={styles.versionTitle}>{versionTitle}</div>
-                                                    <div className={styles.downloadLinks}>
-                                                        {listArr.map(url => {
-                                                            const title = '[' + url.title + ']'
-                                                            return (
-                                                                <a href={url.link}>{title}</a>
-                                                            )
-                                                        })}
-                                                    </div>
-                                                </div>
-                                            )
-                                        })
+                                            </div>
+                                        )
                                     }
                                 </div>
                             )
-                        }
-                    </div>
-                )
+                        })}
+                    </div>)
             })}
         </div>
     )

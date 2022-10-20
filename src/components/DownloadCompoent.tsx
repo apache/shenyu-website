c324ea489355ca6836f3429f0f6279669f79f7b1
hgaol <dhangao@hotmail.com>
2022-10-20 09:24:35 +0800

    using flex wrap layout

--- src/components/DownloadCompoent.tsx
+++ src/components/DownloadCompoent.tsx
@@ -4,9 +4,8 @@ import Translate, { translate } from '@docusaurus/Translate';
 import styles from './DownloadCompoent.module.css';
 
 const data = [
-    [{
-        title: translate({ message: 'ShenYu Source Codes' }),
-        index: 0,
+    {
+        title: translate({ message: 'Source Codes' }),
         versions: [
             {
                 versionTitle: '2.5.0',
@@ -36,7 +35,6 @@ const data = [
     },
     {
         title: translate({ message: 'ShenYu-Admin Binary Distribution' }),
-        index: 1,
         versions: [
             {
                 versionTitle: '2.5.0',
@@ -66,7 +64,6 @@ const data = [
     },
     {
         title: translate({ message: 'ShenYu-Bootstrap Binary Distribution' }),
-        index: 2,
         versions: [
             {
                 versionTitle: '2.5.0',
@@ -93,10 +90,9 @@ const data = [
                 }
             }
         ],
-    }],
-    [{
+    },
+    {
         title: translate({ message: 'ShenYu client golang Source Codes' }),
-        index: 3,
         versions: [
             {
                 versionTitle: '1.0.0',
@@ -107,10 +103,9 @@ const data = [
                 }
             }
         ],
-    }],
-    [{
+    },
+    {
         title: translate({ message: 'ShenYu client .NET Source Codes' }),
-        index: 4,
         versions: [
             {
                 versionTitle: '1.0.0',
@@ -121,7 +116,7 @@ const data = [
                 }
             }
         ],
-    }]
+    }
 ]
 
 function DownloadCompoent() {
@@ -155,50 +150,45 @@ function DownloadCompoent() {
 
     return (
         <div className={styles.downloadContainer}>
-            {data.map((project, index) => {
+            {data.map((item, index) => {
+                const { versions, title } = item;
                 return (
-                    <div key={index} className={styles.downloadRowContainer}>
-                        {project.map((item, index) => {
-                            const { versions, title } = item;
-                            return (
-                                <div key={item.index} className={styles.downloadCard}>
-                                    <div className={styles.downloadCardTitle}>{title}</div>
-                                    <button id='dropDownButton' className={styles.downloadCardButton} onClick={() => { showList(item.index) }}><Translate>Source</Translate></button>
+                    <div className={styles.downloadCard}>
+                        <div className={styles.downloadCardTitle}>{title}</div>
+                        <button id='dropDownButton' className={styles.downloadCardButton} onClick={() => { showList(index) }}><Translate>Source</Translate></button>
+                        {
+                            showItem[index] &&
+                            (
+                                <div className={styles.dropDownContainer}>
                                     {
-                                        showItem[item.index] &&
-                                        (
-                                            <div className={styles.dropDownContainer}>
-                                                {
-                                                    versions.map((item2, index2) => {
-                                                        const { versionTitle, targets } = item2;
-                                                        const listArr = Object.keys(targets).map(list => {
-                                                            return {
-                                                                title: list,
-                                                                link: targets[list],
-                                                            }
-                                                        })
-                                                        return (
-                                                            <div key={index2}>
-                                                                <div className={styles.versionTitle}>{versionTitle}</div>
-                                                                <div className={styles.downloadLinks}>
-                                                                    {listArr.map((url, ind3) => {
-                                                                        const title = '[' + url.title + ']'
-                                                                        return (
-                                                                            <a key={ind3} href={url.link}>{title}</a>
-                                                                        )
-                                                                    })}
-                                                                </div>
-                                                            </div>
-                                                        )
-                                                    })
+                                        versions.map(item2 => {
+                                            const { versionTitle, targets } = item2;
+                                            const listArr = Object.keys(targets).map(list => {
+                                                return {
+                                                    title: list,
+                                                    link: targets[list],
                                                 }
-                                            </div>
-                                        )
+                                            })
+                                            return (
+                                                <div>
+                                                    <div className={styles.versionTitle}>{versionTitle}</div>
+                                                    <div className={styles.downloadLinks}>
+                                                        {listArr.map(url => {
+                                                            const title = '[' + url.title + ']'
+                                                            return (
+                                                                <a href={url.link}>{title}</a>
+                                                            )
+                                                        })}
+                                                    </div>
+                                                </div>
+                                            )
+                                        })
                                     }
                                 </div>
                             )
-                        })}
-                    </div>)
+                        }
+                    </div>
+                )
             })}
         </div>
     )
